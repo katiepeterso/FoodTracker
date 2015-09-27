1244ed31fc841a0b073a74ae454a140a629761be
katiepeterso <katiepeterso@outlook.com>
2015-09-27 11:01:53 -0700

    first commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Katherine Peterson on 2015-09-27.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
