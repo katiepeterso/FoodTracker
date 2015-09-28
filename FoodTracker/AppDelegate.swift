ec82dec1e6c91a6207f1628b6f690d758ca1ec9a
katiepeterso <katiepeterso@outlook.com>
2015-09-28 13:21:51 -0700

    set up parse

--- FoodTracker/AppDelegate.swift
+++ FoodTracker/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // Initialize Parse.
+        
+        Parse.setApplicationId(PARSE_API_APPLICATION_ID,
+            clientKey: PARSE_API_CLIENT_KEY)
+        
         return true
     }
 
--- Podfile
+++ Podfile
@@ -1,3 +1,2 @@
 use_frameworks!
-
 pod 'Parse'
