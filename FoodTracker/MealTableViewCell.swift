ddd696f882a7557ef3a5d9f1feb9e80a79bd3fe5
katiepeterso <katiepeterso@outlook.com>
2015-09-27 17:34:38 -0700

    add table view controller that lists meals and ratings

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Katherine Peterson on 2015-09-27.
+//  Copyright © 2015 KatieExpatriated. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+}
